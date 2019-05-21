872f780901cfc8ce2b60336cda07811fb77f3d00
Nishant Mehta <nishmeht7@gmail.com>
2019-05-20 21:09:14 -0700

    tests folder added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,20 @@
+import argparse
 import shutil
 import gzip
 import filecmp
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from genopheno import preprocess, model, predict
 from os import mkdir
 from os.path import join, exists
 
-
-EXPECTED_DIR = join('resources', 'expected')
+EXPECTED_DIR = join('tests', 'resources', 'expected')
 EXPECTED_PREPROCESS = join(EXPECTED_DIR, 'preprocess')
 EXPECTED_MODEL = join(EXPECTED_DIR, 'model')
 EXPECTED_PREDICT = join(EXPECTED_DIR, 'predict')
-DATA_DIR = join('..', 'genopheno', 'resources', 'data')
+DATA_DIR = join('genopheno', 'resources', 'data')
 USER_DATA_DIR = join(DATA_DIR, 'users')
 SNP_DATA_DIR = join(DATA_DIR, 'snp')
 KNOWN_PHENOTYPES_FILE = join(DATA_DIR, 'known_phenotypes.csv')
@@ -32,15 +36,36 @@ def test_preprocess_eye_color():
             assert exp.readlines() == act.readlines()
 
 
-def test_model_eye_color():
+def test_model_eye_color(ml_model):
     """
     Tests the modeling step.
     """
     __create_dir(OUTPUT_DIR_MODEL)
-    model.run(EXPECTED_PREPROCESS, 50, 80, 15, 33, False, None, 200, 'en', OUTPUT_DIR_MODEL)
-    __assert_files(
-        ['confusion_matrix.txt', 'features.csv', 'model_config.pkl', 'roc.png'], EXPECTED_MODEL, OUTPUT_DIR_MODEL
-    )
+    global EXPECTED_MODEL
+    if ml_model == 'en':
+        EXPECTED_MODEL += "/elasticNet"
+        model.run(EXPECTED_PREPROCESS, 50, 80, 15, 33, False, None, 200, ml_model, 3, OUTPUT_DIR_MODEL)
+        __assert_files(
+            ['confusion_matrix_training_data.txt', 'confusion_matrix_testing_data.txt', 'model_features.csv', 'model_config.pkl', 'roc.png'], EXPECTED_MODEL, OUTPUT_DIR_MODEL
+        )
+    elif ml_model == 'dt':
+        EXPECTED_MODEL += "/decisionTree"
+        model.run(EXPECTED_PREPROCESS, 50, 80, 15, 33, False, None, 200, ml_model, 3, OUTPUT_DIR_MODEL)
+        __assert_files(
+            ['confusion_matrix_training_data.txt', 'confusion_matrix_testing_data.txt', 'dtree.dot', 'dtree.png', 'model_config.pkl'], EXPECTED_MODEL, OUTPUT_DIR_MODEL
+        )
+    elif ml_model == 'rf':
+        EXPECTED_MODEL += "/randomForest"
+        model.run(EXPECTED_PREPROCESS, 50, 80, 15, 33, False, None, 200, ml_model, 3, OUTPUT_DIR_MODEL)
+        __assert_files(
+            ['confusion_matrix_training_data.txt', 'confusion_matrix_testing_data.txt', 'rf_features.csv', 'model_config.pkl', 'rf_features.csv'], EXPECTED_MODEL, OUTPUT_DIR_MODEL
+        )
+    elif ml_model == 'xg':
+        EXPECTED_MODEL += "/xgBoost"
+        model.run(EXPECTED_PREPROCESS, 50, 80, 15, 33, False, None, 200, ml_model, 3, OUTPUT_DIR_MODEL)
+        __assert_files(
+            ['confusion_matrix_training_data.txt', 'confusion_matrix_testing_data.txt', 'xg_features.csv', 'model_config.pkl', 'roc.png'], EXPECTED_MODEL, OUTPUT_DIR_MODEL
+        )
 
 
 def test_predict_eye_color():
@@ -72,3 +97,30 @@ def __create_dir(directory):
     if exists(directory):
         shutil.rmtree(directory)
     mkdir(directory)
+
+def run(model):
+    test_preprocess_eye_color()
+    test_model_eye_color(model)
+    test_predict_eye_color()
+
+if __name__ == '__main__':
+
+    # Parse input
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument(
+        "--model",
+        "-m",
+        default="en",
+        type=str,
+        help="The type of model to use."
+             "\nen = Elastic net"
+             "\ndt = Decision tree"
+             "\nrf = Random Forest"
+             "\nxg = XGboost"
+             "\n\n Default: en"
+    )
+
+    args = parser.parse_args()
+    run(args.model)
+
